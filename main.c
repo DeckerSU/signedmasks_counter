8533991cfdc415d7de5bbce0f48dcc5cc88b9b1e
DeckerSU <support@decker.su>
2018-03-17 18:31:32 +0300

    changes for testnet

--- main.c
+++ main.c
@@ -6,76 +6,77 @@
 
 const char *Notaries_elected[][2] =
 {
-    { "0_jl777_testA", "03b7621b44118017a16043f19b30cc8a4cfe068ac4e42417bae16ba460c80f3828" },
-    { "0_jl777_testB", "02ebfc784a4ba768aad88d44d1045d240d47b26e248cafaf1c5169a42d7a61d344" },
-    { "0_kolo_testA", "0287aa4b73988ba26cf6565d815786caf0d2c4af704d7883d163ee89cd9977edec" },
-    { "artik_AR", "029acf1dcd9f5ff9c455f8bb717d4ae0c703e089d16cf8424619c491dff5994c90" },
-    { "artik_EU", "03f54b2c24f82632e3cdebe4568ba0acf487a80f8a89779173cdb78f74514847ce" },
-    { "artik_NA", "0224e31f93eff0cc30eaf0b2389fbc591085c0e122c4d11862c1729d090106c842" },
-    { "artik_SH", "02bdd8840a34486f38305f311c0e2ae73e84046f6e9c3dd3571e32e58339d20937" },
-    { "badass_EU", "0209d48554768dd8dada988b98aca23405057ac4b5b46838a9378b95c3e79b9b9e" },
-    { "badass_NA", "02afa1a9f948e1634a29dc718d218e9d150c531cfa852843a1643a02184a63c1a7" },
-    { "badass_SH", "026b49dd3923b78a592c1b475f208e23698d3f085c4c3b4906a59faf659fd9530b" },
-    { "crackers_EU", "03bc819982d3c6feb801ec3b720425b017d9b6ee9a40746b84422cbbf929dc73c3" }, // 10
-    { "crackers_NA", "03205049103113d48c7c7af811b4c8f194dafc43a50d5313e61a22900fc1805b45" },
-    { "crackers_SH", "02be28310e6312d1dd44651fd96f6a44ccc269a321f907502aae81d246fabdb03e" },
-    { "durerus_EU", "02bcbd287670bdca2c31e5d50130adb5dea1b53198f18abeec7211825f47485d57" },
-    { "etszombi_AR", "031c79168d15edabf17d9ec99531ea9baa20039d0cdc14d9525863b83341b210e9" },
-    { "etszombi_EU", "0281b1ad28d238a2b217e0af123ce020b79e91b9b10ad65a7917216eda6fe64bf7" },
-    { "etszombi_SH", "025d7a193c0757f7437fad3431f027e7b5ed6c925b77daba52a8755d24bf682dde" },
-    { "farl4web_EU", "0300ecf9121cccf14cf9423e2adb5d98ce0c4e251721fa345dec2e03abeffbab3f" },
-    { "farl4web_SH", "0396bb5ed3c57aa1221d7775ae0ff751e4c7dc9be220d0917fa8bbdf670586c030" },
-    { "fullmoon_AR", "0254b1d64840ce9ff6bec9dd10e33beb92af5f7cee628f999cb6bc0fea833347cc" },
-    { "fullmoon_NA", "031fb362323b06e165231c887836a8faadb96eda88a79ca434e28b3520b47d235b" }, // 20
-    { "fullmoon_SH", "030e12b42ec33a80e12e570b6c8274ce664565b5c3da106859e96a7208b93afd0d" },
-    { "grewal_NA", "03adc0834c203d172bce814df7c7a5e13dc603105e6b0adabc942d0421aefd2132" },
-    { "grewal_SH", "03212a73f5d38a675ee3cdc6e82542a96c38c3d1c79d25a1ed2e42fcf6a8be4e68" },
-    { "indenodes_AR", "02ec0fa5a40f47fd4a38ea5c89e375ad0b6ddf4807c99733c9c3dc15fb978ee147" },
-    { "indenodes_EU", "0221387ff95c44cb52b86552e3ec118a3c311ca65b75bf807c6c07eaeb1be8303c" },
-    { "indenodes_NA", "02698c6f1c9e43b66e82dbb163e8df0e5a2f62f3a7a882ca387d82f86e0b3fa988" },
-    { "indenodes_SH", "0334e6e1ec8285c4b85bd6dae67e17d67d1f20e7328efad17ce6fd24ae97cdd65e" },
-    { "jeezy_EU", "023cb3e593fb85c5659688528e9a4f1c4c7f19206edc7e517d20f794ba686fd6d6" },
-    { "jsgalt_NA", "027b3fb6fede798cd17c30dbfb7baf9332b3f8b1c7c513f443070874c410232446" },
-    { "karasugoi_NA", "02a348b03b9c1a8eac1b56f85c402b041c9bce918833f2ea16d13452309052a982" }, // 30
-    { "kashifali_EU", "033777c52a0190f261c6f66bd0e2bb299d30f012dcb8bfff384103211edb8bb207" },
-    { "kolo_AR", "03016d19344c45341e023b72f9fb6e6152fdcfe105f3b4f50b82a4790ff54e9dc6" },
-    { "kolo_SH", "02aa24064500756d9b0959b44d5325f2391d8e95c6127e109184937152c384e185" },
-    { "metaphilibert_AR", "02adad675fae12b25fdd0f57250b0caf7f795c43f346153a31fe3e72e7db1d6ac6" },
-    { "movecrypto_AR", "022783d94518e4dc77cbdf1a97915b29f427d7bc15ea867900a76665d3112be6f3" },
-    { "movecrypto_EU", "021ab53bc6cf2c46b8a5456759f9d608966eff87384c2b52c0ac4cc8dd51e9cc42" },
-    { "movecrypto_NA", "02efb12f4d78f44b0542d1c60146738e4d5506d27ec98a469142c5c84b29de0a80" },
-    { "movecrypto_SH", "031f9739a3ebd6037a967ce1582cde66e79ea9a0551c54731c59c6b80f635bc859" },
-    { "muros_AR", "022d77402fd7179335da39479c829be73428b0ef33fb360a4de6890f37c2aa005e" },
-    { "noashh_AR", "029d93ef78197dc93892d2a30e5a54865f41e0ca3ab7eb8e3dcbc59c8756b6e355" }, // 40
-    { "noashh_EU", "02061c6278b91fd4ac5cab4401100ffa3b2d5a277e8f71db23401cc071b3665546" },
-    { "noashh_NA", "033c073366152b6b01535e15dd966a3a8039169584d06e27d92a69889b720d44e1" },
-    { "nxtswe_EU", "032fb104e5eaa704a38a52c126af8f67e870d70f82977e5b2f093d5c1c21ae5899" },
-    { "polycryptoblog_NA", "02708dcda7c45fb54b78469673c2587bfdd126e381654819c4c23df0e00b679622" },
-    { "pondsea_AR", "032e1c213787312099158f2d74a89e8240a991d162d4ce8017d8504d1d7004f735" },
-    { "pondsea_EU", "0225aa6f6f19e543180b31153d9e6d55d41bc7ec2ba191fd29f19a2f973544e29d" },
-    { "pondsea_NA", "031bcfdbb62268e2ff8dfffeb9ddff7fe95fca46778c77eebff9c3829dfa1bb411" },
-    { "pondsea_SH", "02209073bc0943451498de57f802650311b1f12aa6deffcd893da198a544c04f36" },
-    { "popcornbag_AR", "02761f106fb34fbfc5ddcc0c0aa831ed98e462a908550b280a1f7bd32c060c6fa3" },
-    { "popcornbag_NA", "03c6085c7fdfff70988fda9b197371f1caf8397f1729a844790e421ee07b3a93e8" }, // 50
-    { "ptytrader_NA", "0328c61467148b207400b23875234f8a825cce65b9c4c9b664f47410b8b8e3c222" },
-    { "ptytrader_SH", "0250c93c492d8d5a6b565b90c22bee07c2d8701d6118c6267e99a4efd3c7748fa4" },
-    { "rnr_AR", "029bdb08f931c0e98c2c4ba4ef45c8e33a34168cb2e6bf953cef335c359d77bfcd" },
-    { "rnr_EU", "03f5c08dadffa0ffcafb8dd7ffc38c22887bd02702a6c9ac3440deddcf2837692b" },
-    { "rnr_NA", "02e17c5f8c3c80f584ed343b8dcfa6d710dfef0889ec1e7728ce45ce559347c58c" },
-    { "rnr_SH", "037536fb9bdfed10251f71543fb42679e7c52308bcd12146b2568b9a818d8b8377" },
-    { "titomane_AR", "03cda6ca5c2d02db201488a54a548dbfc10533bdc275d5ea11928e8d6ab33c2185" },
-    { "titomane_EU", "02e41feded94f0cc59f55f82f3c2c005d41da024e9a805b41105207ef89aa4bfbd" },
-    { "titomane_SH", "035f49d7a308dd9a209e894321f010d21b7793461b0c89d6d9231a3fe5f68d9960" },
-    { "vanbreuk_EU", "024f3cad7601d2399c131fd070e797d9cd8533868685ddbe515daa53c2e26004c3" }, // 60
-    { "xrobesx_NA", "03f0cc6d142d14a40937f12dbd99dbd9021328f45759e26f1877f2a838876709e1" },
-    { "xxspot1_XX", "02ef445a392fcaf3ad4176a5da7f43580e8056594e003eba6559a713711a27f955" },
-    { "xxspot2_XX", "03d85b221ea72ebcd25373e7961f4983d12add66a92f899deaf07bab1d8b6f5573" }
-};
+{
+    "0_jl777_testA", "03b7621b44118017a16043f19b30cc8a4cfe068ac4e42417bae16ba460c80f3828"
+}, {
+    "0_jl777_testB", "02ebfc784a4ba768aad88d44d1045d240d47b26e248cafaf1c5169a42d7a61d344"
+}, {
+    "SHossain_EU", "0239b39630f71b5ef4e145ab9bd9daaf0871fa59904cf9c216619609d2d6ed5ee0"
+}, {
+    "metaphilibert_SH", "0284af1a5ef01503e6316a2ca4abf8423a794e9fc17ac6846f042b6f4adedc3309"
+}, {
+    "webworker01_NA", "032d5e7e1e67d71bb02f95774c3382348c02ff5bd6cb844afb9c57b1eae040b175"
+}, {
+    "voskcoin_NA", "03a47c429b6fd83dc9687ba409ee6f34823094b97bad4c0f4f60649c55bbdf497b"
+}, {
+    "emmanux_EU", "03206f0a4418e3fe985c21ffdd9f883ea58a51c6271c488bb9737fed511ada6bba"
+}, {
+    "meshbits_notarytest_EU", "02463d539fcd89beddf2b4b3e3c1ea141efe8cac4661a5bbebf81e4059c1115af1"
+}, {
+    "adrianpante_NA", "030109570dc62648f5e5d56133a3ccb9cf7e3caa16a4420530b74b137247b77da0"
+}, {
+    "adrianpante_EU", "0384ba5bc403b9a4aa3a5c14f39ebb0e3db057c90a46a2fd80e6fe82dbff6f1658"
+}, {
+    "adrianpante_SH", "03ffc7435e4d7640ca4b589a78f2092b921d379670c5ebd58414846fe9048867a7"
+}, {
+    "adrianpante_AE", "020b2bed39fd504104749368741e0d0521339e3e729331e34f9c9a762a143a99de"
+}, {
+    "infotech-al_NA", "03b381daa68916bb9054520e3cb63abe7970cdb854b0b8ce16f0e3e82b65173867"
+}, {
+    "cipi_EU", "0212666218770ce0f1dada215aafa655a5d8ddee33902ba367f4b7caced4395b6b"
+}, {
+    "peer2cash_AR", "02b5c20deb16e3dd2890d6537c02623077287e14203094a0f63bc3ae026d3dfd65"
+}, {
+    "peer2cash_SH", "02164ac655cf9b37381872c7482983bf0702c2f61bc123be23ddd8b925444c5673"
+}, {
+    "libscott-test_AE", "0367a99aaddbc1603c88678d470aa5e95858f5c1d3a4b74eaacb6770df0a66995b"
+}, {
+    "PTYX_test", "03ea1505756a9bc657930a59d14858efd8dea27782aec556ed81e613c15b8ddbbe"
+}, {
+    "blackjok3r_SH", "0244ef4a6b38ed112a0c50754cfe9eca8ecbef15c87cda86c1b2157e1a4545c962"
+}, {
+    "goldenman_AR", "03c360982f79e6c02ea33d33ffc010d3bbb57edb1659c5b43d354435c2cd3b394d"
+}, {
+    "lukechilds_NA", "0273d3924cf553b5a3baa6d458ecb411558582c3c468fd3ddf5dd29e0e3f1cbb15"
+}, {
+    "decker_test_AR", "0319ac84f941b29f6ed276ebd161772e2e13ad0d51cd834db550deda0b9835f1aa"
+}, {
+    "komodominingpool_test_EU", "023145a94ad53de6f3048aa0c778d6a9f7e3baed4fc8c17756f6f3eb53adc857a4"
+}, {
+    "jorian_test", "032c522148cf737b2864bb4a2729aca3a486a5417c1207f308a6fe5d60f7a864a6"
+}, {
+    "jackson_test", "02a820eac677662587844316ea96b94e48983efb2ce80c2f8f0e1d4a0ee64e5fdc"
+}, {
+    "thegaltmines_test1", "0278397f84cc5614c8f40be4774fd8579a652b755293e7e9ba43577aec99b19e3c"
+}, {
+    "thegaltmines_test2", "031bea28bec98b6380958a493a703ddc3353d7b05eb452109a773eefd15a32e421"
+}, {
+    "thegaltmines_test3", "0352384815030a2b97d518177ee117122e647565f1a03d96d9bd37f8724aa7cfdb"
+}, {
+    "siu_test", "03f82c8c363d23076420a39904f9616d59c26a3d8dc7c51f67d8d3a94335431970"
+}, {
+    "cipi_AR", "020358f7250d4a46568a0df2992117fee765fd7b8fc6f5b14efedbbde1f33beaeb"
+}, {
+    "patchkez_EU", "03461ec21bacdc3f02c2fa8b288824494f67a58ca783dce0897e78f53fdf641b73"
+}};
 
+/*
 char NOTARY_CURRENCIES[][16] = { "USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "NZD",
     "CNY", "RUB", "MXN", "BRL", "INR", "HKD", "TRY", "ZAR", "PLN", "NOK", "SEK", "DKK", "CZK", "HUF", "ILS", "KRW", "MYR", "PHP", "RON", "SGD", "THB", "BGN", "IDR", "HRK",
     "REVS", "SUPERNET", "DEX", "PANGEA", "JUMBLR", "BET", "CRYPTO", "HODL", "SHARK", "BOTS", "MGW", "COQUI", "WLC", "KV", "CEAL", "MESH", "MNZ", "CHIPS", "MSHARK" }; // "LTC",
+*/
 
+char NOTARY_CURRENCIES[][16] = { "PIZZA", "BEER" };
 
 void _iguana_notarystats(char *fname,int32_t totals[64],int32_t dispflag)
 {
